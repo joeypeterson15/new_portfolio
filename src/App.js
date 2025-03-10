14c9d6492c654a42052864cd05f6a50d465d055f
joeypeterson15 <joey.peterson15@gmail.com>
2025-03-10 15:18:39 -0700

    add css intro styling

--- src/App.js
+++ src/App.js
@@ -10,117 +10,40 @@ import {
 } from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
 import { About_Me } from './text/intro_page';
+import { AnimatePresence } from "motion/react"
+import * as motion from "motion/react-client"
+import { container, box, button } from './styles.tsx';
 import Texty from 'rc-texty';
-// import Button from 'antd/lib/button';
 import 'rc-texty/assets/index.css';
-// import TweenOne from 'rc-tween-one';
+import './App.css'
 
 const { Header, Content, Footer, Sider } = Layout;
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
-const items = [
-  getItem('Hey there!', '1', <UserOutlined />),
-  // getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('Projects', 'sub1', <ProjectOutlined />, [
-    getItem('Machine Learning', '3'),
-    getItem('App Clones', '4'),
-    getItem('Visualizations and animations', '5'),
-  ]),
-  getItem('Contact Info', '6', <InfoCircleOutlined />),
 
-  // getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  // getItem('Files', '9', <FileOutlined />),
-];
 const App = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const [show, setShow] = useState(true)
-  const showWelcomeText = () => {
-    setShow(!show)
-  }
+  const [isVisible, setIsVisible] = useState(false)
+
+  const showWelcomeAnim = () => {
+    setIsVisible(!isVisible)
+  };
+
   useEffect(() => {
-    setTimeout(showWelcomeText, 800)
+    setTimeout(showWelcomeAnim, 800)
   }, [])
 
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
   return (
-    <Layout
-      style={{
-        minHeight: '100vh',
-      }}
-    >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            paddingLeft: '40px',
-            background: colorBgContainer,
-          }}
-        >
-          {/* <div className="texty-demo" style={{ marginTop: 16 }}>
-          </div> */}
-          <Texty>{show && 'Ant Motion'}</Texty>
-          {/* {show && (
-            <TweenOne
-              animation={{
-                scale: 1.2,
-                opacity: 1,
-                duration: 500,
-                type: 'from',
-                ease: 'easeOutBack'
-              }}
+      <AnimatePresence id='animate_div' initial={false}>
+          {isVisible ? (
+            <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
+            key="box"
             >
-              <div style={{ width: '100px', height: '100px', backgroundColor: 'red' }}>
-                Animated Element
-              </div>
-            </TweenOne>
-          )} */}
-        </Header>
-        <Content
-          style={{
-            margin: '0 16px',
-            width: '50%'
-            
-          }}
-        >
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}
-          >
-            {/* <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item> */}
-          </Breadcrumb>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-              paddingTop: '20px'
-            }}
-          >
-            <p>{About_Me}</p>
-          </div>
-        </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
-        >
-        </Footer>
-      </Layout>
-    </Layout>
+            <h1 data-text="Welcome to my portfolio! My name is Joey Peterson and I'm a passionate software engineer">Welcome to my portfolio! My name is Joey Peterson and I'm a passionate software engineer</h1>
+            </motion.div>
+          ) : null}
+      </AnimatePresence>
+
   );
 };
 export default App;
---
+++ src/styles.tsx
@@ -0,0 +1,28 @@
+// import React from 'react';
+
+export const container: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "column",
+    width: 100,
+    height: 100,
+    position: "relative",
+}
+
+export const box: React.CSSProperties = {
+    width: '80rem',
+    height: 100,
+    // backgroundColor: "#0cdcf7",
+    backgroundColor: 'white',
+    borderRadius: "10px",
+}
+
+// export const button: React.CSSProperties = {
+//     backgroundColor: "#0cdcf7",
+//     borderRadius: "10px",
+//     padding: "10px 20px",
+//     color: "#0f1115",
+//     position: "absolute",
+//     bottom: 0,
+//     left: 0,
+//     right: 0,
+// }
